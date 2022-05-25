c84dc87b781e282379b416eeff3476388e4500ff
De1shi <106265462+De1shi@users.noreply.github.com>
2022-05-26 00:40:02 +0530

    Add files via upload

---
+++ positive_int_list.py
@@ -0,0 +1,8 @@
+list1 =  [12, -7, 5, 64, -14]
+for number in list1:   
+        if number >=0:
+            print(number)
+list2 = [12, 14, -95, 3]
+for number in list2: 
+        if number >=0:
+            print(number)
