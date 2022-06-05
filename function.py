dcb539742b13148b2b305c4d4a015cbbf35f0032
De1shi <106265462+De1shi@users.noreply.github.com>
2022-06-05 14:23:40 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,21 @@
+string = input("Please enter a word:")
+
+
+def make_dict(x):
+    dictionary = {}
+    for letter in x:
+        dictionary[letter] = 1 + dictionary.get(letter, 0)
+    return dictionary
+
+
+def most_frequent(string):
+    letters = [letter.lower() for letter in string if letter.isalpha()]
+    dictionary = make_dict(letters)
+    result = []
+    for key in dictionary:
+        result.append((dictionary[key], key))
+    result.sort(reverse=True)
+    for count, letter in result:
+        print (letter, count)
+
+most_frequent(string)
